371dcbbf23f8e229f6772442f75082f48f32987e
dubech <franck.dubech@hotmail.com>
2017-03-27 10:24:39 +0200

    Ajout du tableau de base

--- array/app.js
+++ array/app.js
@@ -1,1 +1,6 @@
-var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
+var mois = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'décembre'];
+console.log(mois);
+
+
+
+//var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
