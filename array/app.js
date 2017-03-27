75c397c04408ff92c12e4f78e7ff76bc72dd1f14
dubech <franck.dubech@hotmail.com>
2017-03-27 10:32:42 +0200

    Affiche le l'élément 5

--- array/app.js
+++ array/app.js
@@ -1,5 +1,6 @@
 var mois = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'décembre'];
 console.log(mois[2]);
+console.log(mois[4]);
 
 
 
