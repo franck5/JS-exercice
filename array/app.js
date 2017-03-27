1df0d22c744784df7082bcfa9cdf61905039a804
dubech <franck.dubech@hotmail.com>
2017-03-27 10:34:49 +0200

    affiche novembre

--- array/app.js
+++ array/app.js
@@ -1,6 +1,7 @@
 var mois = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'décembre'];
 console.log(mois[2]);
 console.log(mois[4]);
+console.log(mois[10]);
 
 
 
