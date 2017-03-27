1003ef6383e66f00a98c27c030d7c25012a83ffa
dubech <franck.dubech@hotmail.com>
2017-03-27 14:08:59 +0200

    function compare number

--- functions/app.js
+++ functions/app.js
@@ -3,12 +3,26 @@ function one() {
 }
 console.log(one());
 
+
 function chain(hello) {
 	return hello;
 }
 console.log(chain("Salut"));
 
+
 function twochain(chain1, chain2){
 	return chain1 + " " + chain2
 }
 console.log(twochain("hello", "world"));
+
+
+function comparenumber(number1, number2){
+	if (number1 > number2) {
+		return "Le premier nombre est plus grand";
+	}
+	else if(number1 < number2){
+		return "Le premier nombre est plus petit";
+	}
+	else{
+		return "Les deux nombres sont identiques";
+	}
