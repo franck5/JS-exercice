caae8b161257ea260f06477647cb8ece1e9a6a96
dubech <franck.dubech@hotmail.com>
2017-03-27 10:51:39 +0200

    Correction du mot aout

--- array/app.js
+++ array/app.js
@@ -2,7 +2,8 @@ var mois = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'a
 console.log(mois[2]);
 console.log(mois[4]);
 console.log(mois[10]);
-
+mois.splice(7, 1, "août");
+console.log(mois);
 
 
 //var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
