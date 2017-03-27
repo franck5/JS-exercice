3330100aafab35969248c7405cdc2763679251be
dubech <franck.dubech@hotmail.com>
2017-03-27 10:29:15 +0200

    Affichage du troisième élément du tableau

--- array/app.js
+++ array/app.js
@@ -1,5 +1,5 @@
 var mois = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'décembre'];
-console.log(mois);
+console.log(mois[2]);
 
 
 
