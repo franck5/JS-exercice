0bacb58c416a4c758d161bb07986ff2407480f3a
dubech <franck.dubech@hotmail.com>
2017-03-27 11:08:13 +0200

    fin de l'exo array

--- array/app.js
+++ array/app.js
@@ -9,4 +9,5 @@ var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
 array.push('Courgette');
 array.splice(4, 0, 'Citron');
 array.splice(1,1);
+array.splice(2, 0, 'Poire');
 console.log(array);
